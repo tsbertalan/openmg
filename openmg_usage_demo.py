f4bb2a1c350ef536801d359cf1e0cd65d1ac5356
Tom S. Bertalan <tom@tombertalan.com>
2013-02-08 00:16:36 -0600

    Seems to be sparse, O(N). T ~ c * N. But c is large.
    when sparse matrices are used, behavior seems to be O(N) up to 6000 (50 sec). Will try larger sizes overnight. Patience is low. The Gauss-Seidel solver, however, is quite slow. And the SciPy sparse "direct" solver is super fast. Our Python Gauss-Seidel implementation is probably a main bottleneck, weighted as it is with mandatory memory copies. In production, it could be written in C, C++, or Fortran for great justice. Take off every ZIG.

--- openmg_usage_demo.py
+++ openmg_usage_demo.py
@@ -1,18 +1,20 @@
 import numpy as np
 import openmg as omg
 from scipy import sparse
+from time import time
 
-N = 100
+N = 200
 dense = False  # Should we use dense matrices for multigrid?
                # This is not useful for small problems like N=100
 
-A = np.random.rand(N, N)  # this will make the multigrid algorithm diverge!
+#A = np.random.rand(N, N)  # this will make the multigrid algorithm diverge!
                           # (although the pure-iterative params will still work)
-A = omg.poisson1D(N)  # a dense array for the 1D Poisson equation
+if dense:
+    A = omg.poisson1D(N)  # a dense array for the 1D Poisson equation
+else:
+    A = omg.poisson(N)    # sparse 
 
-if not dense:
-    A = sparse.csr_matrix(A)
-u_true = np.random.rand(N)
+u_true = np.array([np.sin(x / 10.0) for x in range(N)])
 if dense:
     b = np.dot(A, u_true)
 else:
@@ -22,22 +24,31 @@ else:
 # view the docstring
 #help(omg.mg_solve)
 
-# use only the iterative solver:
-params = {'problemshape': (N,), 'gridlevels': 1,
-          'iterations': 2000,    'verbose': False}
+# use only the coarse solver:
+params = {'problemshape': (N,), 'gridlevels': 1, 'verbose': False}
+a = time()
 soln  = omg.mg_solve(A, b, params)[0]
-#print params
-print "error norm (Gauss-Seidel):", np.linalg.norm(soln - u_true)
-# output:
-#  error norm (Gauss-Seidel): 1.0511248589e-13
+elapsed = time() - a
+# scipy's sparse solver is a very fast direct solver for this problem:
+print "error norm (direct method):", np.linalg.norm(soln - u_true), "in", elapsed, "seconds."
+# error norm (direct method): 3.84255102999e-15 in 0.00324702262878 seconds.
 
+# This Gauss-Seidel solver will be painfully slow for N larger than, say 200.
+# It's probably the main bottleneck in this process, and really should be written in C.
+a = time()
+soln  = omg.iterative_solve(A, b, np.zeros((N, 1)), 200, verbose=False)[0]
+elapsed = time() - a
+print "error norm (Gauss-Seidel):", np.linalg.norm(soln - u_true), "in", elapsed, "seconds."
+# error norm (Gauss-Seidel): 22.4064222452 in 6.83140087128 seconds.
 
 # use a 2-grid pattern
-params = {'problemshape': (N,), 'gridlevels': 2, 'cycles': 25,
-          'iterations': 1,       'verbose': False, 'dense': dense}
+g=3
+params = {'problemshape': (N,), 'gridlevels': g, 'cycles': 10,
+          'iterations': 2,       'verbose': False, 'dense': dense}
+a = time()
 soln  = omg.mg_solve(A, b, params)[0]
+elapsed = time() - a
 #print params
-print "error norm (2-grid):", np.linalg.norm(soln - u_true)
-# output:
-#  error norm (2-grid): 3.49272429615e-12
+print "error norm (%i-grid):"%g, np.linalg.norm(soln - u_true), "in", elapsed, "seconds."
+# error norm (2-grid): 1.63537088016e-15 in 9.87337112427 seconds.
 
