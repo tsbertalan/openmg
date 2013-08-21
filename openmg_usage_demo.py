815a942cb9964e0959e5372b7fe3f4359b3890e2
Tom Bertalan <tom@tombertalan.com>
2013-08-21 12:27:29 -0400

    a little better formatting in the usage demo

--- openmg_usage_demo.py
+++ openmg_usage_demo.py
@@ -5,54 +5,62 @@ from time import time
 from sys import argv
 
 def main(N):
-    dense = False  # Should we use dense matrices for multigrid?
-                # This is not useful for small problems like N=100
-
-    #A = np.random.rand(N, N)  # this will make the multigrid algorithm diverge!
-                            # (although the pure-iterative params will still work)
-    u_true = np.array([np.sin(x / 10.0) for x in range(N)])
-    if dense:
-        A = omg.poisson1D(N)  # a dense array for the 1D Poisson equation
-        b = np.dot(A, u_true)
-    else:
-        A = omg.poisson(N)    # sparse 
-        b = A * u_true
-
-    threshold = 1e-10
-
-    # view the docstring
+    ## view the docstring
     #help(omg.mg_solve)
 
-    # use only the coarse solver:
-    params = {'problemshape': (N,), 'gridlevels': 1, 'verbose': False, 'threshold': threshold}
-    a = time()
+    
+    ## Set up some parameters that will be true for each example.    
+    dense = False  ## Should we use dense matrices for multigrid?
+                   ## This is not useful for small problems like N=100
+    threshold = 1e-10
+
+
+    ## Set up the problem.
+    #A = np.random.rand(N, N)  ## this will make the multigrid algorithm diverge!
+                               ## (although the pure-iterative params will still work)
+    u_true = np.array([np.sin(x / 10.0) for x in range(N)])
+    if dense:
+        A = omg.poisson1D(N)  ## a dense array for the 1D Poisson equation
+        b = np.dot(A, u_true)
+    else:
+        A = omg.poisson(N)    ## sparse 
+        b = A * u_true
+
+
+
+    ## Use only the coarse solver.
+    params = {'verbose': False, 'threshold': threshold}
+    start = time()
     soln  = omg.coarse_solve(A, b)
-    elapsed = time() - a
-    # scipy's sparse solver is a very fast direct solver for this problem:
+    elapsed = time() - start
     print N, "direct", np.linalg.norm(omg.getresidual(b, A, soln, N)), elapsed
-    # error norm (direct method): 3.84255102999e-15 in 0.00324702262878 seconds.
 
-    # This Gauss-Seidel solver will be painfully slow for N larger than, say 200.
-    # It's probably the main bottleneck in this process, and really should be written in C.
+
+    ## Use an iterative solver.
+    ##   This Gauss-Seidel solver will be painfully slow for N larger than, say 200.
+    ##   It's probably the main bottleneck in this process, and really should be written in C.
     if N <= 200:
-        a = time()
-        soln  = omg.iterative_solve_to_threshold(A, b, np.zeros((N, 1)), threshold, verbose=False)[0]
-        elapsed = time() - a
+        start = time()
+        soln = omg.iterative_solve_to_threshold(A, b, np.zeros((N, 1)),
+                                                params['threshold'],
+                                                verbose=params['verbose'])[0]
+        elapsed = time() - start
         print N, "Gauss-Seidel", np.linalg.norm(omg.getresidual(b, A, soln, N)), elapsed
-        # error norm (Gauss-Seidel): 22.4064222452 in 6.83140087128 seconds.
 
-    # use a 2-grid pattern
-    g = 3  # number of gridlevels
-    params = {'problemshape': (N,), 'gridlevels': g, 'cycles': 10,
-            'iterations': 2,       'verbose': False, 'dense': dense, 'threshold': threshold}
-    a = time()
+
+    ## Use a 2-grid pattern.
+    params = {'problemshape': (N,), 'gridlevels': 3, 'cycles': 10,
+              'iterations': 2,       'verbose': False, 'dense': dense, 'threshold': threshold}
+    start = time()
     mg_output  = omg.mg_solve(A, b, params)
     soln = mg_output[0]
     cycles = mg_output[1]['cycle']
-    elapsed = time() - a
+    elapsed = time() - start
     #print params
-    print N, "%i-grid"%g, np.linalg.norm(omg.getresidual(b, A, soln, N)), elapsed, cycles
-    # error norm (2-grid): 1.63537088016e-15 in 9.87337112427 seconds.
+    print N, "%i-grid"%params['gridlevels'],\
+          np.linalg.norm(omg.getresidual(b, A, soln, N)), elapsed, cycles
+
+
 
 if __name__=="__main__":
     args = [int(i) for i in argv[1:]]
